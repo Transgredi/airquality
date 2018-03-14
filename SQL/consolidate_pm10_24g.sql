5ee6f8bf881466fca072572e05a97145b1be5dc8
Transgredi <transgredi@gmail.com>
2018-03-14 18:43:59 +0100

    Fixing CSV files and adding SQL file

--- SQL/consolidate_pm10_24g.sql
+++ SQL/consolidate_pm10_24g.sql
@@ -3,3 +3,13 @@ SELECT * FROM 2000_pm10_24g LIMIT 100;
 SELECT COLUMN_NAME FROM information_schema.columns WHERE table_schema = 'airquality' AND table_name = '2000_pm10_24g' AND column_name != 'Kod stacji';
 
 SELECT GROUP_CONCAT(COLUMN_NAME) FROM information_schema.columns WHERE table_schema = 'airquality' AND table_name = '2000_pm10_24g' AND column_name != 'Kod stacji';
+
+SELECT MAX(ORDINAL_POSITION) FROM information_schema.columns WHERE table_schema = 'airquality' AND table_name = '2000_pm10_24g';
+
+
+
+SELECT
+	`Kod stacji`, 
+	(SELECT COLUMN_NAME FROM information_schema.columns WHERE table_name = '2000_pm10_24g' AND ORDINAL_POSITION = 2) AS Station, 
+    DsWrocWie 
+FROM 2000_pm10_24g;
