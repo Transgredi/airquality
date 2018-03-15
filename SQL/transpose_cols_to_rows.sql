56f0055587b3dbba53578b630ed35cb1149bdd3a
Transgredi <transgredi@gmail.com>
2018-03-15 14:15:24 +0100

    Working on transposing cols to rows

--- SQL/transpose_cols_to_rows.sql
+++ SQL/transpose_cols_to_rows.sql
@@ -9,15 +9,10 @@ SELECT
       c.column_name,
       ' as Pollution_Level FROM 2000_pm10_24g
       '
-    ) SEPARATOR ' UNION ALL '
-  ) INTO @sql
-FROM information_schema.columns c
-where c.table_name = '2000_pm10_24g' AND c.column_name != 'Kod stacji'
-order by c.ordinal_position;
+    ) SEPARATOR ' UNION ALL ') INTO @sql
+FROM information_schema.columns c WHERE c.table_name = '2000_pm10_24g' AND c.column_name != 'Kod stacji' ORDER BY c.ordinal_position;
 
-SET @sql = CONCAT('SELECT `Kod stacji`, Station_Code, Pollution_Level FROM (\n\t', @sql, ') x');
-
-SELECT @sql;
+SET @sql = CONCAT('SELECT `Kod stacji`, Station_Code, Pollution_Level FROM (\n\t', @sql, ') U');
 
 PREPARE stmt FROM @sql;
 EXECUTE stmt;
