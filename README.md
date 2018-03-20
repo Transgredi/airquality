6f81865e86a6d94862734895d0b00c6443a3bb74
Transgredi <transgredi@gmail.com>
2018-03-20 21:22:04 +0100

    Readme file

---
+++ README.md
@@ -0,0 +1,8 @@
+# Datasources
+1. asdaa
+2. asdasd
+3. 
+# Steps
+1. asdasd 
+2. asdasda
+3. 
--- SQL/calculating_date_elements.sql
+++ SQL/calculating_date_elements.sql
@@ -1,1 +1,4 @@
-SELECT *, dayname(Observation_Date), dayofmonth(Observation_Date), weekday(Observation_Date)+1 FROM airquality.airquality;
+SELECT *, YEAR(Observation_Date), (Observation_Date), dayofmonth(Observation_Date), weekday(Observation_Date)+1 FROM airquality.airquality LIMIT 5000;
+
+
+UPDATE airquality SET Year_Number = YEAR(Observation_Date);
