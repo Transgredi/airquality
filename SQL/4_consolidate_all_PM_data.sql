3dec4b5b910c59d179f553c3afa4928d5f7ced77
Transgredi <transgredi@gmail.com>
2018-04-09 21:48:36 +0200

    ...

--- SQL/4_consolidate_all_PM_data.sql
+++ SQL/4_consolidate_all_PM_data.sql
@@ -4,22 +4,22 @@ DELETE FROM PM25_1g_cons WHERE Measure_Date IN ('Wskaźnik', 'Czas uśredniania'
 DELETE FROM PM25_24g_cons WHERE Measure_Date IN ('Wskaźnik', 'Czas uśredniania', 'Czas pomiaru');
 
 TRUNCATE airquality;
-INSERT INTO airquality
+INSERT INTO airquality;
 SELECT 
 	uuid_generate_v4() AS uid, 
     str_to_date(Measure_Date,'%m/%d/%Y %H:%i') AS observation_date,
     Aggregation AS granulation,
     Station_Code AS station_code,
     Pollution_Type AS pollution_type,
-    TRUNCATE(REPLACE((CASE Pollution_Level WHEN '' THEN NULL ELSE Pollution_Level END), ',', '.'), 2) AS pollution,
+    TRUNCATE(REPLACE((CASE Pollution_Level WHEN '' THEN NULL ELSE Pollution_Level END), ',', '.'), 2) AS pollution
 		FROM (
-		SELECT Measure_Date, Station_Code, Pollution_Level, 'PM10' AS Pollution_Type, '1g' AS Aggregation FROM PM10_1g_cons
+		SELECT Measure_Date, Station_Code, Pollution_Level, 'PM10' AS Pollution_Type, '1g' AS Aggregation FROM PM10_1h_cons
 		UNION ALL
-		SELECT Measure_Date, Station_Code, Pollution_Level, 'PM10' AS Pollution_Type, '24g' AS Aggregation FROM PM10_24g_cons
+		SELECT Measure_Date, Station_Code, Pollution_Level, 'PM10' AS Pollution_Type, '24g' AS Aggregation FROM PM10_24h_cons
 		UNION ALL
-		SELECT Measure_Date, Station_Code, Pollution_Level, 'PM2.5' AS Pollution_Type, '1g' AS Aggregation FROM PM25_1g_cons
+		SELECT Measure_Date, Station_Code, Pollution_Level, 'PM2.5' AS Pollution_Type, '1g' AS Aggregation FROM PM25_1h_cons
 		UNION ALL
-		SELECT Measure_Date, Station_Code, Pollution_Level, 'PM2.5' AS Pollution_Type, '24g' AS Aggregation FROM PM25_24g_cons
+		SELECT Measure_Date, Station_Code, Pollution_Level, 'PM2.5' AS Pollution_Type, '24g' AS Aggregation FROM PM25_24h_cons
 		) PD WHERE Measure_Date != '';
         
 
--- SQL/5_create_views.sql
+++ SQL/5_create_views.sql
@@ -8,3 +8,122 @@ SELECT DISTINCT
 	street
 FROM station_metadata
 WHERE status = 'aktywny';
+
+
+DROP VIEW IF EXISTS PM_Wro;
+CREATE VIEW PM_Wro AS
+SELECT * FROM airquality 
+WHERE station_code IN (SELECT code FROM station_metadata WHERE city = 'Wrocław');
+
+
+DROP VIEW IF EXISTS airquality_pm10_1h;
+CREATE VIEW airquality_pm10_1h AS
+SELECT 
+	a.observation_date,
+	a.pollution_type,
+	a.granulation,
+	a.pollution,
+	a.station_code,
+	s.voivodship, 
+	s.city,
+	s.street
+FROM airquality a
+LEFT JOIN station_metadata s ON a.station_code = s.code
+WHERE 
+	a.granulation = '1h'
+	AND a.pollution_type = 'PM10'
+ORDER BY a.observation_date;
+
+
+DROP VIEW IF EXISTS airquality_pm10_24h;
+CREATE VIEW airquality_pm10_24h AS
+SELECT 
+	a.observation_date,
+	a.pollution_type,
+	a.granulation,
+	a.pollution,
+	a.station_code,
+	s.voivodship, 
+	s.city,
+	s.street
+FROM airquality a
+LEFT JOIN station_metadata s ON a.station_code = s.code
+WHERE 
+	a.granulation = '24h'
+	AND a.pollution_type = 'PM10'
+ORDER BY a.observation_date;
+
+
+DROP VIEW IF EXISTS airquality_pm25_1h;
+CREATE VIEW airquality_pm25_1h AS
+SELECT 
+	a.observation_date,
+	a.pollution_type,
+	a.granulation,
+	a.pollution,
+	a.station_code,
+	s.voivodship, 
+	s.city,
+	s.street
+FROM airquality a
+LEFT JOIN station_metadata s ON a.station_code = s.code
+WHERE 
+	a.granulation = '1h'
+	AND a.pollution_type = 'PM2.5'
+ORDER BY a.observation_date;
+
+
+DROP VIEW IF EXISTS airquality_pm25_24h;
+CREATE VIEW airquality_pm25_24h AS
+SELECT 
+	a.observation_date,
+	a.pollution_type,
+	a.granulation,
+	a.pollution,
+	a.station_code,
+	s.voivodship, 
+	s.city,
+	s.street
+FROM airquality a
+LEFT JOIN station_metadata s ON a.station_code = s.code
+WHERE 
+	a.granulation = '24h'
+	AND a.pollution_type = 'PM2.5'
+ORDER BY a.observation_date;
+
+
+
+/* 
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * SEPARATE VIEWS FOR EACH GRANULATION AND POLLUTION TYPE FOR WROCLAW ONLY
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
+ */
+
+DROP VIEW IF EXISTS PM_Wro_PM10_1h;
+CREATE VIEW PM_Wro_PM10_1h AS
+SELECT * FROM airquality
+WHERE station_code IN (SELECT code FROM station_metadata WHERE city = 'Wrocław')
+AND pollution_type = 'PM10' AND granulation = '1h';
+
+
+DROP VIEW IF EXISTS PM_Wro_PM10_24h;
+CREATE VIEW PM_Wro_PM10_24h AS
+SELECT * FROM airquality
+WHERE station_code IN (SELECT code FROM station_metadata WHERE city = 'Wrocław')
+AND pollution_type = 'PM10' AND granulation = '24h';
+
+
+DROP VIEW IF EXISTS PM_Wro_PM25_1h;
+CREATE VIEW PM_Wro_PM25_1h AS
+SELECT * FROM airquality
+WHERE station_code IN (SELECT code FROM station_metadata WHERE city = 'Wrocław')
+AND pollution_type = 'PM2.5' AND granulation = '1h';
+
+
+DROP VIEW IF EXISTS PM_Wro_PM25_24h;
+CREATE VIEW PM_Wro_PM25_24h AS
+SELECT * FROM airquality
+WHERE station_code IN (SELECT code FROM station_metadata WHERE city = 'Wrocław')
+AND pollution_type = 'PM2.5' AND granulation = '24h';
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
