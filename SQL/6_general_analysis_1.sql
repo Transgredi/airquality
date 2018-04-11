c5fb8649e291cf0ea249f1809dd95601ca884824
Transgredi <transgredi@gmail.com>
2018-04-11 14:51:00 +0200

    ...

--- SQL/6_general_analysis_1.sql
+++ SQL/6_general_analysis_1.sql
@@ -1,28 +1,26 @@
 SELECT
-	EXTRACT(YEAR FROM observation_date) AS yr, 	
-	EXTRACT(MONTH FROM observation_date) AS mth,
-	station_code,
-	MAX(pollution) AS pollution
-FROM public.airquality
+	CAST(EXTRACT(YEAR FROM observation_date) AS INT) AS yr, 	
+	CAST(EXTRACT(MONTH FROM observation_date) AS INT) AS mth,
+	AVG(pollution) AS pollution
+FROM airquality
 WHERE 
 	pollution_type = 'PM10' 
 	AND granulation = '24h' 
 	AND station_code LIKE '%Wroc%' 
 	AND EXTRACT(YEAR FROM observation_date) > 2014
-GROUP BY ROLLUP(yr, mth, station_code)
-ORDER BY yr, mth, station_code;
+GROUP BY ROLLUP(yr, mth)
+ORDER BY yr, mth;
 
 
 SELECT
 	EXTRACT(YEAR FROM observation_date) AS yr, 	
 	EXTRACT(MONTH FROM observation_date) AS mth,
-	station_code,
 	pollution_type,
 	MAX(pollution) AS pollution
-FROM public.airquality
+FROM airquality
 WHERE 
 	granulation = '24h' 
 	AND station_code LIKE '%Wroc%' 
 	AND EXTRACT(YEAR FROM observation_date) > 2014
-GROUP BY CUBE(yr, mth, station_code, pollution_type)
-ORDER BY yr, mth, station_code, pollution_type;
+GROUP BY CUBE(yr, mth, pollution_type)
+ORDER BY yr, mth, pollution_type;
