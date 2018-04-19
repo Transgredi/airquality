d5fd0bb78f09bbfb3b630830512c8ac307249d93
Transgredi <transgredi@gmail.com>
2018-04-19 19:23:05 +0200

    generating time series

---
+++ SQL/8_generate_ts.sql
@@ -0,0 +1,7 @@
+SELECT CURRENT_DATE + i FROM GENERATE_SERIES(date '2010-01-01' - CURRENT_DATE,
+	date '2010-05-01' - CURRENT_DATE) i;
+	
+
+SELECT DATE_TRUNC('hour', HH)::timestamp
+FROM GENERATE_SERIES('2010-01-01'::timestamp, '2010-02-01'::timestamp,
+	'1 hour'::INTERVAL) HH;
