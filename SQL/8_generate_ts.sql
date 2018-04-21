2dcfb68492362216a43bc110c99ded311cbc3e1d
Transgredi <transgredi@gmail.com>
2018-04-21 21:42:41 +0200

    generating time series table

--- SQL/8_generate_ts.sql
+++ SQL/8_generate_ts.sql
@@ -1,3 +1,5 @@
+-- https://stackoverflow.com/questions/14113469/generating-time-series-between-two-dates-in-postgresql
+
 SELECT CURRENT_DATE + i FROM GENERATE_SERIES(date '2010-01-01' - CURRENT_DATE,
 	date '2010-05-01' - CURRENT_DATE) i;
 	
@@ -5,3 +7,10 @@ SELECT CURRENT_DATE + i FROM GENERATE_SERIES(date '2010-01-01' - CURRENT_DATE,
 SELECT DATE_TRUNC('hour', HH)::timestamp
 FROM GENERATE_SERIES('2010-01-01'::timestamp, '2010-02-01'::timestamp,
 	'1 hour'::INTERVAL) HH;
+	
+
+
+SELECT * INTO ts_interval_1h FROM (
+	SELECT DATE_TRUNC('hour', HH)::timestamp
+FROM GENERATE_SERIES('2010-01-01'::timestamp, '2010-02-01'::timestamp,
+	'1 hour'::INTERVAL) HH) AS ts;
